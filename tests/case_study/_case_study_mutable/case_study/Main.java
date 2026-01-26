777620dc90035b12d04cbb0775f7cdc5679bf62c
Florian Lanzinger <lanzinger@kit.edu>
2026-01-26 21:57:04 +0100

    Case study proofs

--- tests/case_study/_case_study_mutable/case_study/Main.java
+++ tests/case_study/_case_study_mutable/case_study/Main.java
@@ -20,18 +20,30 @@ public final class Main {
         @NonNull @AgedOver(age="18") Customer customer18 = new Customer("Alice", 18);
         Shop shop = new Shop();
 
-        shop.addOrder(new Order(18, customer18, product18));
+        addOrderHelper(shop, 18, customer18, product18);
 
         @NonNull @AllowedFor(age="6") Product product6 = new Product("Tim & Jeffrey, All Episodes", 10, 6);
-        shop.addOrder(new Order(14, customer18, product6));
+        addOrderHelper(shop, 14, customer18, product6);
 
         @NonNull @AgedOver(age="14") Customer customer14 = new Customer("Bob", 14);
-        shop.addOrder(new Order(14, customer14, product6));
+        addOrderHelper(shop, 14, customer14, product6);
 
         shop.processNextOrder();
         shop.processNextOrder();
         shop.processNextOrder();
 
         shop.processNextOrder();
+    }
+
+    @JMLClauseTranslationOnly("requires \\invariant_for(shop);")
+    @JMLClauseTranslationOnly("ensures \\invariant_for(shop);")
+    @JMLClauseTranslationOnly("assignable shop.orders, shop.orders.footprint;")
+    // These errors are reported by the type checker, but mended by the SMT solver, so there remains nothing to be proven in KeY
+    // :: error: agedover.contracts.postcondition.not.satisfied :: error: allowedfor.contracts.postcondition.not.satisfied
+    public static void addOrderHelper(@Unique Shop shop, int witness, @AgedOver(age="witness") Customer customer, @AllowedFor(age="witness") Product product) {
+        // :: error: agedover.argument.type.incompatible :: error: allowedfor.argument.type.incompatible
+        shop.addOrder(new Order(witness, customer, product));
+        Assert.immutableFieldUnchanged_TranslationOnly("customer", "customer.age");
+        Assert.immutableFieldUnchanged_TranslationOnly("product", "product.ageRestriction");
     }
 }
